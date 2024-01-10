5bf065afece3f9b9b30e879e3864aba8732569b6
AndreyZaklitskiy <andreyzaklitskiy@gmail.com>
2024-01-11 01:24:17 +0200

    add server response parsing

--- src/js/main.js
+++ src/js/main.js
@@ -54,3 +54,93 @@ function overlayFadeOutAnimation(element, fadeIn, fadeOut) {
         element.classList.add(fadeIn)
     }
 }
+
+
+const serverResponse = [
+    {
+      "CustomerName": "Jane Cooper",
+      "Company": "Microsoft",
+      "PhoneNumber": "(225) 555-0118",
+      "Email": "jane@microsoft.com",
+      "Country": "United States",
+      "Status": "Active"
+    },
+    {
+      "CustomerName": "Floyd Miles",
+      "Company": "Yahoo",
+      "PhoneNumber": "(205) 555-0100",
+      "Email": "floyd@yahoo.com",
+      "Country": "Kiribati",
+      "Status": "Inactive"
+    },
+    {
+      "CustomerName": "Ronald Richards",
+      "Company": "Adobe",
+      "PhoneNumber": "(302) 555-0107",
+      "Email": "ronald@adobe.com",
+      "Country": "Israel",
+      "Status": "Inactive"
+    },
+    {
+      "CustomerName": "Marvin McKinney",
+      "Company": "Tesla",
+      "PhoneNumber": "(252) 555-0126",
+      "Email": "marvin@tesla.com",
+      "Country": "Iran",
+      "Status": "Active"
+    },
+    {
+      "CustomerName": "Jerome Bell",
+      "Company": "Google",
+      "PhoneNumber": "(629) 555-0129",
+      "Email": "jerome@google.com",
+      "Country": "Réunion",
+      "Status": "Active"
+    },
+    {
+      "CustomerName": "Kathryn Murphy",
+      "Company": "Microsoft",
+      "PhoneNumber": "(406) 555-0120",
+      "Email": "kathryn@microsoft.com",
+      "Country": "Curaçao",
+      "Status": "Active"
+    },
+    {
+      "CustomerName": "Jacob Jones",
+      "Company": "Yahoo",
+      "PhoneNumber": "(208) 555-0112",
+      "Email": "jacob@yahoo.com",
+      "Country": "Brazil",
+      "Status": "Active"
+    },
+    {
+      "CustomerName": "Kristin Watson",
+      "Company": "Facebook",
+      "PhoneNumber": "(704) 555-0127",
+      "Email": "kristin@facebook.com",
+      "Country": "Åland Islands",
+      "Status": "Inactive"
+    }
+  ]
+  const wrap = document.querySelector('.customers-list__data-wrap');
+  for (const dataObject of serverResponse) {
+    const dataRow = document.createElement("tr");
+    dataRow.className = 'customers-list__row customers-list__row_data';
+    for (const key of Object.keys(dataObject)) {
+        const td = document.createElement('td');
+        const span = document.createElement('span');
+        td.className = 'customers-list__col';
+        span.className = 'customers-list__text';
+        span.textContent = dataObject[key];
+        if (key === 'Status') {
+            const statusSpan = document.createElement('span');
+            statusSpan.className = `customers-list__text customers-list__status customers-list__status_${dataObject[key].toLowerCase()}`;
+            statusSpan.textContent = dataObject[key];
+            td.appendChild(statusSpan);
+        }else {
+            td.appendChild(span);
+        }
+        dataRow.appendChild(td);
+    }
+    wrap.appendChild(dataRow);
+  }
